3ece5c656a5c81d5d3efb60c2fb10697330d76f8
Marlin314 <meller@uw.edu>
2026-01-06 22:24:27 -0800

    Click to create a Square

--- src/sandbox/Squares.java
+++ src/sandbox/Squares.java
@@ -3,25 +3,41 @@ import react.graphics.*;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 public class Squares extends WinApp{
   public static G.VS theVS = new G.VS(100,100,200,300);
   public static Color color = G.rndColor();
+  public static Square.List squares = new Square.List();
 
   public Squares(){super("Squares",1000,800);}
 
   @Override
   public void paintComponent(Graphics g){
     G.clearBack(g);
-    theVS.fill(g, color); // give us a nice Rect
+    squares.draw(g);
   }
 
   @Override
   public void mousePressed(MouseEvent me){
-    if(theVS.hit(me.getX(), me.getY())){color = G.rndColor();}
-    repaint(); // don't forget to repaint when you change something.
+    squares.add(new Square(me.getX(), me.getY()));
+    repaint();
   }
   
   public static void main(String[] args){PANEL=new Squares();WinApp.launch();}
+
+  //-----------------Square------------------------------
+  public static class Square extends G.VS{
+    public Color c = G.rndColor();
+    public Square(int x, int y){super(x,y,100,100);}
+    public void draw(Graphics g){fill(g,c);}
+
+    //------------------List----------------------------
+    public static class List extends ArrayList<Square> {
+      public void draw(Graphics g){for(Square s : this){s.draw(g);}}
+    }
+  }
+
+
 }
 
